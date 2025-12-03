50ed2e2174d7fef268735f8fab35d60358f19c4e
Trevor Wilson <kmdreko@gmail.com>
2025-12-02 22:28:34 -0700

    chore: expand benchmarks

--- venator-engine/benches/my_benchmark.rs
+++ venator-engine/benches/my_benchmark.rs
@@ -6,7 +6,7 @@ use venator_engine::filter::{FilterPredicate, Order, Query};
 use venator_engine::storage::{FileStorage, TransientStorage};
 use venator_engine::Timestamp;
 
-fn event_counts_benchmark(c: &mut Criterion) {
+fn count_events_benchmark(c: &mut Criterion) {
     let file_storage = FileStorage::new(Path::new("./benches/test.vena.db"));
     let file_engine = SyncEngine::new(file_storage).unwrap();
 
@@ -14,54 +14,97 @@ fn event_counts_benchmark(c: &mut Criterion) {
     file_engine.copy_dataset(&mut mem_storage).unwrap();
     let mem_engine = SyncEngine::new(mem_storage).unwrap();
 
+    let make_query = |q_str| Query {
+        filter: FilterPredicate::parse(q_str).unwrap(),
+        order: Order::Asc,
+        limit: 0,
+        start: Timestamp::MIN,
+        end: Timestamp::MAX,
+        previous: None,
+    };
+
     drop(file_engine);
 
-    let query = Query {
-        filter: FilterPredicate::parse("").unwrap(),
-        order: Order::Asc,
-        limit: 0,
-        start: Timestamp::MIN,
-        end: Timestamp::MAX,
-        previous: None,
-    };
-
-    c.bench_function("count all events", |b| {
-        b.iter(|| {
-            assert_eq!(
-                black_box(mem_engine.query_event_count(query.clone())),
-                16537
-            )
-        })
+    let q_str = "";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 16537))
     });
 
-    let query = Query {
-        filter: FilterPredicate::parse("#level: >=INFO").unwrap(),
-        order: Order::Asc,
-        limit: 0,
-        start: Timestamp::MIN,
-        end: Timestamp::MAX,
-        previous: None,
-    };
-
-    c.bench_function("count all events with level", |b| {
-        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(query.clone())), 1511))
+    let q_str = "#level: >=TRACE";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 16537))
     });
 
-    let query = Query {
-        filter: FilterPredicate::parse("#level: >=INFO #content: /^logging in/").unwrap(),
-        order: Order::Asc,
-        limit: 0,
-        start: Timestamp::MIN,
-        end: Timestamp::MAX,
-        previous: None,
-    };
+    let q_str = "#level: >=INFO";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 1511))
+    });
 
-    c.bench_function("count all events with level and regex", |b| {
-        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(query.clone())), 874))
+    let q_str = "#level: >=TRACE @http.status_code: 200";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 14990))
+    });
+
+    let q_str = "#level: >=TRACE @http.status_code: !200";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 1547))
+    });
+
+    let q_str = "#level: >=TRACE @http.status_code: >200";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 1377))
+    });
+
+    let q_str = "#level: >=TRACE @http.method: POST";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 9261))
+    });
+
+    let q_str = "#level: >=TRACE @http.method: !POST";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 7276))
+    });
+
+    let q_str = "#level: >=TRACE @http.status_code: 200 @http.method: POST";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 7884))
+    });
+
+    let q_str = "#level: >=INFO #content: /^creating/";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 23))
+    });
+
+    let q_str = "#level: >=TRACE #content: /request$/";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 15026))
+    });
+
+    let q_str = "#level: >=INFO #content: creating*";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 23))
+    });
+
+    let q_str = "#level: >=TRACE #content: *request";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count events: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_event_count(q.clone())), 15026))
     });
 }
 
-fn span_counts_benchmark(c: &mut Criterion) {
+fn count_spans_benchmark(c: &mut Criterion) {
     let file_storage = FileStorage::new(Path::new("./benches/test.vena.db"));
     let file_engine = SyncEngine::new(file_storage).unwrap();
 
@@ -69,47 +112,77 @@ fn span_counts_benchmark(c: &mut Criterion) {
     file_engine.copy_dataset(&mut mem_storage).unwrap();
     let mem_engine = SyncEngine::new(mem_storage).unwrap();
 
+    let make_query = |q_str| Query {
+        filter: FilterPredicate::parse(q_str).unwrap(),
+        order: Order::Asc,
+        limit: 0,
+        start: Timestamp::MIN,
+        end: Timestamp::MAX,
+        previous: None,
+    };
+
     drop(file_engine);
 
-    let query = Query {
-        filter: FilterPredicate::parse("").unwrap(),
-        order: Order::Asc,
-        limit: 0,
-        start: Timestamp::MIN,
-        end: Timestamp::MAX,
-        previous: None,
-    };
-
-    c.bench_function("count all spans", |b| {
-        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(query.clone())), 98197))
+    let q_str = "";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 98197))
     });
 
-    let query = Query {
-        filter: FilterPredicate::parse("#level: >=INFO").unwrap(),
-        order: Order::Asc,
-        limit: 0,
-        start: Timestamp::MIN,
-        end: Timestamp::MAX,
-        previous: None,
-    };
-
-    c.bench_function("count all spans with level", |b| {
-        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(query.clone())), 98197))
+    let q_str = "#level: >=TRACE";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 98197))
     });
 
-    let query = Query {
-        filter: FilterPredicate::parse("#level: >=INFO @service.name: /^a/").unwrap(),
-        order: Order::Asc,
-        limit: 0,
-        start: Timestamp::MIN,
-        end: Timestamp::MAX,
-        previous: None,
-    };
+    let q_str = "#level: >=INFO";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 98197))
+    });
 
-    c.bench_function("count all spans with level and regex", |b| {
-        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(query.clone())), 97704))
+    let q_str = "#level: >=TRACE @http.status_code: 200";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 93084))
+    });
+
+    let q_str = "#level: >=TRACE @http.status_code: !200";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 5113))
+    });
+
+    let q_str = "#level: >=TRACE @http.status_code: >200";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 4620))
+    });
+
+    let q_str = "#level: >=TRACE @http.method: POST";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 48133))
+    });
+
+    let q_str = "#level: >=TRACE @http.method: !POST";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 50064))
+    });
+
+    let q_str = "#level: >=TRACE @http.status_code: 200 @http.method: POST";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 43513))
+    });
+
+    let q_str = "#level: >=TRACE @service.name: /^a/";
+    let q = make_query(q_str);
+    c.bench_function(&format!("count spans: {q_str}"), |b| {
+        b.iter(|| assert_eq!(black_box(mem_engine.query_span_count(q.clone())), 97704))
     });
 }
 
-criterion_group!(benches, event_counts_benchmark, span_counts_benchmark);
+criterion_group!(benches, count_events_benchmark, count_spans_benchmark);
 criterion_main!(benches);
